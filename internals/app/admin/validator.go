30c399b982a01e2d0ddad9633c77c2eb97b0deb6
aparnasukesh <aparnasukesh0723@gmail.com>
2024-09-11 18:48:06 +0530

    movie schedule crud completed

--- internals/app/admin/validator.go
+++ internals/app/admin/validator.go
@@ -20,22 +20,16 @@ func ValidateAdmin(user Admin) error {
 			switch fieldName {
 			case "Email":
 				errorMessages[i] = "Invalid Email"
-				break
 			case "Username":
 				errorMessages[i] = "Invalid Username, Minimum 8 letters or Maximum 24 letters required"
-				break
 			case "FirstName":
 				errorMessages[i] = "Invalid Firstname,  Minimum 4 letters or Maximum 10 letters requird "
-				break
 			case "LastName":
 				errorMessages[i] = "Invalid Lastname, Minimum 4 letters or Maximum 10 letters required "
-				break
 			case "Password":
 				errorMessages[i] = "Invalid password, Minimum 6 letters or Maximum 12 letters required"
-				break
 			case "PhoneNumber":
 				errorMessages[i] = "Invalid Phone Number"
-				break
 			default:
 				errorMessages[i] = "Validation failed"
 			}
