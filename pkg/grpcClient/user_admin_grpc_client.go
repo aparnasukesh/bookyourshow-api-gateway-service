10d9cccb4178eadb5ec75d8ef1b1337bd4acb180
aparnasukesh <aparnasukesh0723@gmail.com>
2024-11-22 21:24:15 +0530

    grpc local host changed

--- pkg/grpcClient/user_admin_grpc_client.go
+++ pkg/grpcClient/user_admin_grpc_client.go
@@ -1,29 +1,48 @@
 package grpcclient
 
 import (
+	"log"
+
 	pb "github.com/aparnasukesh/inter-communication/user_admin"
 	"google.golang.org/grpc"
 )
 
+// func NewUserGrpcClient(port string) (pb.UserServiceClient, error) {
+// 	conn, err := grpc.Dial("user-admin-svc:"+port, grpc.WithInsecure(),grpc.WithDefaultServiceConfig({"loadBalancingPolicy":"round_robin"}))
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return pb.NewUserServiceClient(conn), nil
+// }
+
 func NewUserGrpcClient(port string) (pb.UserServiceClient, error) {
-	conn, err := grpc.Dial("user-admin-svc:"+port, grpc.WithInsecure())
+	address := "user-admin-svc.svc.cluster.local:" + port
+	serviceConfig := `{"loadBalancingPolicy": "round_robin"}`
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithDefaultServiceConfig(serviceConfig))
 	if err != nil {
+		log.Printf("Failed to connect to gRPC service: %v", err)
 		return nil, err
 	}
 	return pb.NewUserServiceClient(conn), nil
 }
 
 func NewAdminGrpcClient(port string) (pb.AdminServiceClient, error) {
-	conn, err := grpc.Dial("user-admin-svc:"+port, grpc.WithInsecure())
+	address := "user-admin-svc.svc.cluster.local:" + port
+	serviceConfig := `{"loadBalancingPolicy": "round_robin"}`
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithDefaultServiceConfig(serviceConfig))
 	if err != nil {
+		log.Printf("Failed to connect to gRPC service: %v", err)
 		return nil, err
 	}
 	return pb.NewAdminServiceClient(conn), nil
 }
 
 func NewSuperAdminServiceClient(port string) (pb.SuperAdminServiceClient, error) {
-	conn, err := grpc.Dial("user-admin-svc:"+port, grpc.WithInsecure())
+	address := "user-admin-svc.svc.cluster.local:" + port
+	serviceConfig := `{"loadBalancingPolicy": "round_robin"}`
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithDefaultServiceConfig(serviceConfig))
 	if err != nil {
+		log.Printf("Failed to connect to gRPC service: %v", err)
 		return nil, err
 	}
 	return pb.NewSuperAdminServiceClient(conn), nil
