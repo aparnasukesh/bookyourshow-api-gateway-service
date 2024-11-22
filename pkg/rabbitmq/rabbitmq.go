3f849514b75a3484cc09552686d52e81eee9eb3e
aparnasukesh <aparnasukesh0723@gmail.com>
2024-11-22 19:45:04 +0530

    cicd completed

--- pkg/rabbitmq/rabbitmq.go
+++ pkg/rabbitmq/rabbitmq.go
@@ -7,6 +7,7 @@ import (
 //	func NewRabbitMQConnection() (*amqp.Connection, error) {
 //		return amqp.Dial("amqp://guest:guest@rabbitmq:5672/") // Change localhost to rabbitmq
 //	}
+
 func NewRabbitMQConnection() (*amqp.Connection, error) {
 	return amqp.Dial("amqp://user:password@rabbitmq:5672/") // Use updated credentials
 }
