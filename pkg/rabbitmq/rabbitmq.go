a214bec4de043d338af5d275551c8eef2fb16e7d
aparnasukesh <aparnasukesh0723@gmail.com>
2025-03-25 01:06:08 +0530

    movies-booking grpc client host name and address changed

--- pkg/rabbitmq/rabbitmq.go
+++ pkg/rabbitmq/rabbitmq.go
@@ -1,15 +1,12 @@
 package rabbitmq
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/streadway/amqp"
 )
 
-// func NewRabbitMQConnection() (*amqp.Connection, error) {
-// 	return amqp.Dial("amqp://guest:guest@rabbitmq:5672/") // Change localhost to rabbitmq
-// }
+func NewRabbitMQConnection() (*amqp.Connection, error) {
+	return amqp.Dial("amqp://guest:guest@rabbitmq:5672/") // Change localhost to rabbitmq
+}
 
 // func NewRabbitMQConnection() (*amqp.Connection, error) {
 // 	return amqp.Dial("amqp://user:password@rabbitmq:5672/") // Use updated credentials
@@ -19,10 +16,10 @@ import (
 // 	return amqp.Dial("amqp://myuser:mypassword@rabbitmq:5672/")
 // }
 
-func NewRabbitMQConnection() (*amqp.Connection, error) {
-	username := os.Getenv("RABBITMQ_USERNAME")
-	password := os.Getenv("RABBITMQ_PASSWORD")
+// func NewRabbitMQConnection() (*amqp.Connection, error) {
+// 	username := os.Getenv("RABBITMQ_USERNAME")
+// 	password := os.Getenv("RABBITMQ_PASSWORD")
 
-	rabbitmqURL := fmt.Sprintf("amqp://%s:%s@rabbitmq:5672/", username, password)
-	return amqp.Dial(rabbitmqURL)
-}
+// 	rabbitmqURL := fmt.Sprintf("amqp://%s:%s@rabbitmq:5672/", username, password)
+// 	return amqp.Dial(rabbitmqURL)
+// }
