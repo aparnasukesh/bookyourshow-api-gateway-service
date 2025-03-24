d7240941f970cf494ec9688b132484cfd8cf5d8a
aparnasukesh <aparnasukesh0723@gmail.com>
2025-03-24 16:02:10 +0530

    kubernetes file and cicd changed

--- pkg/rabbitmq/rabbitmq.go
+++ pkg/rabbitmq/rabbitmq.go
@@ -4,10 +4,10 @@ import (
 	"github.com/streadway/amqp"
 )
 
-// func NewRabbitMQConnection() (*amqp.Connection, error) {
-// 	return amqp.Dial("amqp://guest:guest@rabbitmq:5672/") // Change localhost to rabbitmq
-// }
-
 func NewRabbitMQConnection() (*amqp.Connection, error) {
-	return amqp.Dial("amqp://user:password@rabbitmq:5672/") // Use updated credentials
+	return amqp.Dial("amqp://guest:guest@rabbitmq:5672/") // Change localhost to rabbitmq
 }
+
+// func NewRabbitMQConnection() (*amqp.Connection, error) {
+// 	return amqp.Dial("amqp://user:password@rabbitmq:5672/") // Use updated credentials
+// }
