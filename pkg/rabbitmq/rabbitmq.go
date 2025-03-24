9ecf99ea84ac8d0432e1e63ff8381410721c152b
aparnasukesh <aparnasukesh0723@gmail.com>
2025-03-25 01:18:46 +0530

    namespace changed to default

--- pkg/rabbitmq/rabbitmq.go
+++ pkg/rabbitmq/rabbitmq.go
@@ -11,15 +11,3 @@ func NewRabbitMQConnection() (*amqp.Connection, error) {
 // func NewRabbitMQConnection() (*amqp.Connection, error) {
 // 	return amqp.Dial("amqp://user:password@rabbitmq:5672/") // Use updated credentials
 // }
-
-// func NewRabbitMQConnection() (*amqp.Connection, error) {
-// 	return amqp.Dial("amqp://myuser:mypassword@rabbitmq:5672/")
-// }
-
-// func NewRabbitMQConnection() (*amqp.Connection, error) {
-// 	username := os.Getenv("RABBITMQ_USERNAME")
-// 	password := os.Getenv("RABBITMQ_PASSWORD")
-
-// 	rabbitmqURL := fmt.Sprintf("amqp://%s:%s@rabbitmq:5672/", username, password)
-// 	return amqp.Dial(rabbitmqURL)
-// }
