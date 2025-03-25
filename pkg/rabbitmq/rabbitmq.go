68a82971bb8f4287bd9313b56014cab58ed29b18
aparnasukesh <aparnasukesh0723@gmail.com>
2025-03-25 12:22:58 +0530

    namespace changed to default

--- pkg/rabbitmq/rabbitmq.go
+++ pkg/rabbitmq/rabbitmq.go
@@ -1,13 +1,14 @@
 package rabbitmq
 
-import (
-	"github.com/streadway/amqp"
-)
-
-func NewRabbitMQConnection() (*amqp.Connection, error) {
-	return amqp.Dial("amqp://guest:guest@rabbitmq:5672/") // Change localhost to rabbitmq
-}
+import "github.com/streadway/amqp"
 
 // func NewRabbitMQConnection() (*amqp.Connection, error) {
-// 	return amqp.Dial("amqp://user:password@rabbitmq:5672/") // Use updated credentials
+// 	return amqp.Dial("amqp://guest:guest@rabbitmq:5672/") // Change localhost to rabbitmq
 // }
+
+//	func NewRabbitMQConnection() (*amqp.Connection, error) {
+//		return amqp.Dial("amqp://user:password@rabbitmq:5672/") // Use updated credentials
+//	}
+func NewRabbitMQConnection() (*amqp.Connection, error) {
+	return amqp.Dial("amqp://myuser:mypassword@rabbitmq:5672/")
+}
