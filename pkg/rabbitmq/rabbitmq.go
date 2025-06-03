98937feb4c487f2b8a55d768f1c2a7091b4bfe3f
aparnasukesh <aparnasukesh0723@gmail.com>
2025-06-03 23:31:35 +0530

    rabbitmq connection updated

--- pkg/rabbitmq/rabbitmq.go
+++ pkg/rabbitmq/rabbitmq.go
@@ -1,6 +1,12 @@
 package rabbitmq
 
-import "github.com/streadway/amqp"
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/streadway/amqp"
+)
 
 // func NewRabbitMQConnection() (*amqp.Connection, error) {
 // 	return amqp.Dial("amqp://guest:guest@rabbitmq:5672/") // Change localhost to rabbitmq
@@ -10,6 +16,22 @@ import "github.com/streadway/amqp"
 //		return amqp.Dial("amqp://user:password@rabbitmq:5672/") // Use updated credentials
 //	}
 
+//	func NewRabbitMQConnection() (*amqp.Connection, error) {
+//		return amqp.Dial("amqp://admin:admin123@rabbitmq:5672/") // Updated creadentials
+//	}
 func NewRabbitMQConnection() (*amqp.Connection, error) {
-	return amqp.Dial("amqp://admin:admin123@rabbitmq:5672/") // Updated creadentials
+	var conn *amqp.Connection
+	var err error
+
+	for i := 0; i < 10; i++ {
+		conn, err = amqp.Dial("amqp://admin:admin123@rabbitmq:5672/")
+		if err == nil {
+			log.Println("✅ Connected to RabbitMQ")
+			return conn, nil
+		}
+		log.Printf("❌ RabbitMQ not ready, retrying in 5s... (%d/10) - %v\n", i+1, err)
+		time.Sleep(5 * time.Second)
+	}
+
+	return nil, fmt.Errorf("failed to connect to RabbitMQ after retries: %v", err)
 }
