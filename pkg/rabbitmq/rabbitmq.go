c54b547ed6f0bf0d550c3caf20cb70deb79e67bc
aparnasukesh <aparnasukesh0723@gmail.com>
2025-06-03 20:07:48 +0530

    new aws ac created

--- pkg/rabbitmq/rabbitmq.go
+++ pkg/rabbitmq/rabbitmq.go
@@ -2,13 +2,14 @@ package rabbitmq
 
 import "github.com/streadway/amqp"
 
-// func NewRabbitMQConnection() (*amqp.Connection, error) {
-// 	return amqp.Dial("amqp://guest:guest@rabbitmq:5672/") // Change localhost to rabbitmq
-// }
+func NewRabbitMQConnection() (*amqp.Connection, error) {
+	return amqp.Dial("amqp://guest:guest@rabbitmq:5672/") // Change localhost to rabbitmq
+}
 
 //	func NewRabbitMQConnection() (*amqp.Connection, error) {
 //		return amqp.Dial("amqp://user:password@rabbitmq:5672/") // Use updated credentials
 //	}
-func NewRabbitMQConnection() (*amqp.Connection, error) {
-	return amqp.Dial("amqp://myuser:mypassword@rabbitmq:5672/")
-}
+
+// func NewRabbitMQConnection() (*amqp.Connection, error) {
+// 	return amqp.Dial("amqp://myuser:mypassword@rabbitmq:5672/")
+// }
